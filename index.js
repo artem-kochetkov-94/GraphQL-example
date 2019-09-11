9104d00d43949e937ccc2e2d81e170653d8af224
Artem <artem-kochetkov-94@mail.ru>
2019-09-11 17:41:00 +0300

    доработки, добавлен тип User, DateTime, тегирование

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const { ApolloServer } = require("apollo-server");
+const { GraphQLScalarType } = require("graphql");
 
 const typeDefs = `
+    enum PhotoCategory {
+      SELFIE
+      PORTRAIT
+      ACTION
+      LANDSCAPE
+      GRAPHIC
+    }
+
+    scalar DateTime
+
     type Photo {
         id: ID!
         url: String!
         name: String!
         description: String
+        category: PhotoCategory!
+        postedBy: User!
+        taggedUsers: [User!]!
+        created: DateTime!
+    }
+
+    input PostPhotoInput {
+      name: String!
+      category: PhotoCategory=PORTRAIT
+      description: String
+    }
+
+    type User {
+      githubLogin: ID!
+      name: String
+      avatar: String
+      postedPhotos: [Photo!]!
+      inPhotos: [Photo!]!
     }
 
     type Query {
@@ -14,33 +43,97 @@ const typeDefs = `
     }
 
     type Mutation {
-        postPhoto(name: String! description: String): Photo!
+        postPhoto(input: PostPhotoInput!): Photo!
     }
 `;
 
 let _id = 0;
 
-const photos = [];
+const users = [
+  { githubLogin: "mHattrup", name: "Mike Hattrup" },
+  { githubLogin: "gPlake", name: "Glen Plake" },
+  { githubLogin: "sSchmidt", name: "Scot Schmidt" }
+];
+
+const photos = [
+  {
+    id: "1",
+    name: "Dropping the Heart Chute",
+    description: "The heart chute is one of my favorite chutes",
+    category: "ACTION",
+    githubUser: "gPlake",
+    created: "3-28-1977"
+  },
+  {
+    id: "2",
+    name: "Enjoying the sunshine",
+    category: "SELFIE",
+    githubUser: "sSchmidt",
+    created: "1-2-1985"
+  },
+  {
+    id: "3",
+    name: "Gunbarrel 25",
+    description: "25 laps on gunbarrel today",
+    category: "LANDSCAPE",
+    githubUser: "sSchmidt",
+    created: "2018-04-15T19:09:57.308Z"
+  }
+];
+
+const tags = [
+  { photoID: "1", userID: "gPlake" },
+  { photoID: "2", userID: "sSchmidt" },
+  { photoID: "2", userID: "mHattrup" },
+  { photoID: "2", userID: "gPlake" }
+];
 
 const resolvers = {
   Query: {
     totalPhotos: () => photos.length,
     allPhotos: () => photos
   },
-
   Mutation: {
     postPhoto(parent, args) {
       const newPhoto = {
         id: _id++,
-        ...args
+        ...args.input,
+        created: new Date()
       };
       photos.push(newPhoto);
       return newPhoto;
     }
   },
   Photo: {
-    url: parent => `http://yoursite.com/img/${parent.id}.jpg`
-  }
+    url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
+    postedBy: parent => {
+      return users.find(u => u.githubLogin === parent.githubUser);
+    },
+    taggedUsers: parent => {
+      return tags
+        .filter(tag => tag.photoID === parent.id)
+        .map(tag => tag.userID)
+        .map(userID => users.find(u => u.githubLogin === userID));
+    }
+  },
+  User: {
+    postedPhotos: parent => {
+      return photos.filter(p => p.githubUser === parent.githubLogin);
+    },
+    inPhotos: parent => {
+      return tags
+        .filter(tag => tags.userID === parent.id)
+        .map(tag => tag.photoID)
+        .map(photoID => photos.find(p => p.id === photoID));
+    }
+  },
+  DateTime: new GraphQLScalarType({
+    name: "DateTime",
+    description: "A valid date time value.",
+    parseValue: value => new Date(value),
+    serialize: value => new Date(value).toISOString(),
+    parseLiteral: ast => ast.value
+  })
 };
 
 const server = new ApolloServer({
