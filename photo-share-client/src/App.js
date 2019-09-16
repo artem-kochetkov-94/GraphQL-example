b27ed484f9c2fb12fbb42e341536ca69df0c819d
Artem <artem-kochetkov-94@mail.ru>
2019-09-16 17:22:43 +0300

    авторизация

--- photo-share-client/src/App.js
+++ photo-share-client/src/App.js
@@ -1,16 +1,31 @@
 import React from "react";
 import Users from "./Users";
+import { BrowserRouter } from "react-router-dom";
 import { gql } from "apollo-boost";
+import AuthorizedUser from "./AuthorizedUser";
 
 export const ROOT_QUERY = gql`
   query allUsers {
     totalUsers
     allUsers {
-      githubLogin
-      name
-      avatar
+      ...userInfo
+    }
+    me {
+      ...userInfo
     }
   }
+
+  fragment userInfo on User {
+    githubLogin
+    name
+    avatar
+  }
 `;
-const App = () => <Users />;
+const App = () => (
+  <BrowserRouter>
+    <AuthorizedUser />
+    <Users />
+  </BrowserRouter>
+);
+
 export default App;
